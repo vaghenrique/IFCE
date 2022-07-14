dfbc7318bb9a424ea8b1db48686cd5a6721f8bdd
Vagner Henrique <vagner@lit.ifce.edu.br>
2022-07-13 23:20:47 -0300

    primeiro

---
+++ src/main/java/com/mycompany/teste/Numero.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.teste;
+
+/**
+ *
+ * @author henri
+ */
+public class Numero {
+    
+    public long fatorial(int valor){
+        if(valor == 0){
+            return 1;
+        }else{
+            return valor * fatorial(valor - 1);
+        }
+    }
+    
+    public static void main(String vags[]){
+        Numero num = new Numero();
+        System.out.println("valor: "+num.fatorial(10));
+    }
+}
